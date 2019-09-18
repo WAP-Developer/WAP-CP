47ceb8f3c3f1f58c9874b23e58f2381cfc9a748f
Aldi Wiguna <12172979@bsi.ac.id>
2019-09-18 22:45:59 +0700

    Customize Mobile Organization View

--- application/views/home/organization.php
+++ application/views/home/organization.php
@@ -8,11 +8,12 @@
         </div>
     </div>
 
-    <section id="speakers">
+    <section id="organization">
         <div class="container">
             <div class="row">
+                <!-- Mobile -->
                 <?php for ($i = 1; $i <= 8; $i++) { ?>
-                    <div class="col-3">
+                    <div class="col-6 mobile">
                         <div class="member-profile">
                             <div class="unhover_img">
                                 <img src="<?= base_url('assets/img/speaker-3-hover.png'); ?>" alt="" />
@@ -25,6 +26,24 @@
                         </div>
                     </div>
                 <?php } ?>
+                <!-- Mobile End -->
+
+                <!-- Desktop -->
+                <?php for ($i = 1; $i <= 8; $i++) { ?>
+                    <div class="col-3 desktop">
+                        <div class="member-profile">
+                            <div class="unhover_img">
+                                <img src="<?= base_url('assets/img/speaker-3-hover.png'); ?>" alt="" />
+                            </div>
+                            <div class="hover_img">
+                                <img src="<?= base_url('assets/img/speaker-3-hover.png'); ?>" alt="" />
+                            </div>
+                            <span>Web Designer</span>
+                            <h4><span>Teresa</span> Crawford</h4>
+                        </div>
+                    </div>
+                <?php } ?>
+                <!-- Desktop END -->
 
             </div>
     </section>
